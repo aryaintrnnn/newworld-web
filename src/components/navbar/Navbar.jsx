3c2de6c6b7145169b7cc08b8a56d3304616e7491
intrn <aryaintaran24@gmail.com>
2023-05-30 23:51:05 +0800

    Revision Photo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -11,25 +11,25 @@ const Navbar = () => {
         </div>        
         <div className="containerNav">
             <div className="navMenuKanan">
-                <a href="#home" onClick={() => setIcon("#home")} className={icon == "#home" ? "iconAktif" : ""} >
+                <a href="#home" onClick={() => setIcon("#home")} className={icon === "#home" ? "iconAktif" : ""} >
                     <i class="uil uil-house-user navIcon"></i>
                     <span className='navText'>Home</span>
                 </a>
-                <a href="#about" onClick={() => setIcon("#about")} className={icon == "#about" ? "iconAktif" : ""} >
+                <a href="#about" onClick={() => setIcon("#about")} className={icon === "#about" ? "iconAktif" : ""} >
                     <i class="uil uil-cloud-info navIcon"></i>
                     <span className='navText'>Features</span> 
                 </a>
-                <a href="#work" onClick={() => setIcon("#work")} className={icon == "#work" ? "iconAktif" : ""} >
+                <a href="#work" onClick={() => setIcon("#work")} className={icon === "#work" ? "iconAktif" : ""} >
                     <i class="uil uil-archive navIcon"></i>    
                     <span className='navText'>Rules</span> 
                 </a>
             </div>
             <div className="navMenuBawah">
-                <a href="#service" onClick={() => setIcon("#service")} className={icon == "#service" ? "iconAktif" : ""} >
+                <a href="#service" onClick={() => setIcon("#service")} className={icon === "#service" ? "iconAktif" : ""} >
                     <i class="uil uil-apps navIcon"></i>
                     <span className='navText'>Connect</span> 
                 </a>
-                <a href="#contact" onClick={() => setIcon("#contact")} className={icon == "#contact" ? "iconAktif" : ""} >
+                <a href="#contact" onClick={() => setIcon("#contact")} className={icon === "#contact" ? "iconAktif" : ""} >
                     <i class="uil uil-mobile-android navIcon"></i>
                     <span className='navText'>Discord</span> 
                 </a>
