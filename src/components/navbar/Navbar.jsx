29d6d1c3c5b872a9a175273045e56dda2fe4af55
aryaintrnnn <aryaintaran24@gmail.com>
2023-05-30 11:49:23 +0800

    Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,13 @@
 import {useState} from 'react'
 import "./navbar.css"
+import Logo from "../../assets/imgs/logo.png"
 
 const Navbar = () => {
     const [icon, setIcon] = useState("#home")
   return (
     <nav>
         <div className="navUtama">
-            <span>Welcome to</span>
-            <span>Arya Intaran Website</span>
+            <img className='logoImg' src={Logo} alt="" />
         </div>        
         <div className="containerNav">
             <div className="navMenuKanan">
@@ -17,21 +17,21 @@ const Navbar = () => {
                 </a>
                 <a href="#about" onClick={() => setIcon("#about")} className={icon == "#about" ? "iconAktif" : ""} >
                     <i class="uil uil-cloud-info navIcon"></i>
-                    <span className='navText'>About</span> 
+                    <span className='navText'>Features</span> 
                 </a>
                 <a href="#work" onClick={() => setIcon("#work")} className={icon == "#work" ? "iconAktif" : ""} >
                     <i class="uil uil-archive navIcon"></i>    
-                    <span className='navText'>Work</span> 
+                    <span className='navText'>Rules</span> 
                 </a>
             </div>
             <div className="navMenuBawah">
                 <a href="#service" onClick={() => setIcon("#service")} className={icon == "#service" ? "iconAktif" : ""} >
                     <i class="uil uil-apps navIcon"></i>
-                    <span className='navText'>Service</span> 
+                    <span className='navText'>Connect</span> 
                 </a>
                 <a href="#contact" onClick={() => setIcon("#contact")} className={icon == "#contact" ? "iconAktif" : ""} >
                     <i class="uil uil-mobile-android navIcon"></i>
-                    <span className='navText'>Contact</span> 
+                    <span className='navText'>Discord</span> 
                 </a>
             </div>
         </div>
