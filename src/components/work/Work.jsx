44c8c06a4450d9a72a78884e125cc597badad4ff
intrn <aryaintaran24@gmail.com>
2023-05-31 10:25:35 +0800

    Rules Pages Part 1

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -5,28 +5,51 @@ import WorkBaju from "../../assets/imgs/workbaju.png"
 const Work = () => {
   return (
     <section id='work'>
-        <div className="kerjaLayout">
-            <div className="kerjaTypo">
-                <span>Portofolio</span>
-                <span>Portofolio</span>
-                <span>Portofolio</span>
-                <span>Portofolio</span>
+        <div className="rules">
+            <div className="rulesTxt">
+                <span>- Rules -</span>
             </div>
-            <span className="kerjaBuka">Featured Project</span>
-        </div>
-        <div className="containerKerja">
-            <div className="kerja">
-                <img src={WorkBaju} alt="" className="kerjaImg" />
-                <div className="kerjaTxt">
-                    <span className="jmlhKerja">01.</span>
-                    <div className="judulKerja">
-                        <span>Groovey Clothing Brand</span>
-                        <span>Website catalog for a local clothing brand</span>
-                    </div>
+            <div className="rulesB">
+                <div className="rulesA">
+                    <h5>General Rules</h5>
+                </div>
+                <div className="rulesC">
+                    <p>
+                        Mic is required to enter the New World server
+                    </p>
+                    <p>
+                        Everyone has a different RP mindset. 
+                        Don't dictate how others should roleplay
+                    </p>
                 </div>
             </div>
-            <div className="kerjaLink">
-                <a href="" className="linkKerja"><i class="uil uil-arrow-up-left"></i><span>Check Out My Work </span></a>
+            <div className="rulesB">
+                <div className="rulesA">
+                    <h5>General Rules</h5>
+                </div>
+                <div className="rulesC">
+                    <p>
+                        Mic is required to enter the New World server
+                    </p>
+                    <p>
+                        Everyone has a different RP mindset. 
+                        Don't dictate how others should roleplay
+                    </p>
+                </div>
+            </div>
+            <div className="rulesB">
+                <div className="rulesA">
+                    <h5>General Rules</h5>
+                </div>
+                <div className="rulesC">
+                    <p>
+                        Mic is required to enter the New World server
+                    </p>
+                    <p>
+                        Everyone has a different RP mindset. 
+                        Don't dictate how others should roleplay
+                    </p>
+                </div>
             </div>
         </div>
     </section>
