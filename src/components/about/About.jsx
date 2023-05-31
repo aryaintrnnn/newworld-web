a3865568424243d22bbd462127e1d72ebe7af253
intrn <aryaintaran24@gmail.com>
2023-05-31 10:02:44 +0800

    Feature Pages

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,14 +4,43 @@ import "./about.css"
 const About = () => {
   return (
     <section className='about' id='about'>
-      <div className="aboutText">
-        <span className="aboutMeText">about me</span>
-        <span className="aboutMeText">about me</span>
-        <span className="aboutMeText">about me</span>
-      </div>
-      <div className="aboutIsi">
-        <h1 className="abtIsi">Hello, my name is Arya Ngurah Intaran, I like to create various kinds of websites, currently I am studying for an undergraduate degree at ITB Stikom Bali</h1>
-        <a className='btnAbout' href="#">Read More <i class="uil uil-arrow-up-right"></i></a>
+      <div className="fitur">
+        <div className="tittleBar">
+          <span>- Our Features -</span>
+        </div>
+        <div className="box">
+          <div className="kartu">
+            <h5><i class="uil uil-microphone"></i>Voice In-Game</h5>
+          </div>
+          <div className="kartu">
+            <h5><i class="uil uil-university"></i>Robbery System</h5>
+          </div>
+          <div className="kartu">
+            <h5><i class="uil uil-bag"></i>Whitelist Jobs</h5>
+          </div>
+        </div>
+        <div className="box">
+          <div className="kartu">
+            <h5><i class="uil uil-suitcase"></i>Non Whitelist Jobs</h5>
+          </div>
+          <div className="kartu">
+            <h5><i class="uil uil-ankh"></i>Custom Peds</h5>
+          </div>
+          <div className="kartu">
+            <h5><i class="uil uil-setting"></i>Optimize FPS</h5>
+          </div>
+        </div>
+        <div className="box">
+          <div className="kartu">
+            <h5><i class="uil uil-signal"></i>Low Ping And PL</h5>
+          </div>
+          <div className="kartu">
+            <h5><i class="uil uil-layer-group"></i>Max 100 Player</h5>
+          </div>
+          <div className="kartu">
+            <h5><i class="uil uil-apps"></i>Many Features</h5>
+          </div>
+        </div>
       </div>
     </section>
   )
